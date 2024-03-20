45e54cf8e2245b9b7fe10b462557a2a541a4d87e
Stuart Osborn <stuartgosborn22@gmail.com>
2024-03-20 04:52:32 -0500

    added type defs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,32 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    skills: [String]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    addSkill(userId: ID!, skill: String!): User
+    removeUser: User
+    removeSkill(skill: String!): User
+  }
+`;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
